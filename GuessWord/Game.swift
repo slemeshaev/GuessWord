2afa00ba6163fc92cf4e6ded1842660720a6f4ca
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2021-12-08 22:27:13 +0300

    Added Main screen of the game
    Added game model for convenient storage of game state.

---
+++ GuessWord/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  GuessWord
+//
+//  Created by Stanislav Lemeshaev on 07.12.2021.
+//  Copyright © 2021 slemeshaev. All rights reserved.
+//
+
+struct Game {
+    var hiddenWord: String
+    var wrongMovesRemainder: Int
+}
