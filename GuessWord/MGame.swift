17804a6b90d1deea925bae90b0b3d8f62c5e6032
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2021-12-15 22:25:23 +0300

    Added MGame game model - Added game model to manage its state; - Added a method to update the correct word; - Replaced access modifiers with private.

---
+++ GuessWord/MGame.swift
@@ -0,0 +1,46 @@
+//
+//  MGame.swift
+//  GuessWord
+//
+//  Created by Stanislav Lemeshaev on 07.12.2021.
+//  Copyright © 2021 slemeshaev. All rights reserved.
+//
+
+struct MGame {
+    
+    // MARK: - Init
+    init(hiddenWord: String, wrongMovesRemainder: Int) {
+        self.hiddenWord = hiddenWord
+        self.wrongMovesRemainder = wrongMovesRemainder
+    }
+    
+    // MARK: - Properties
+    var hiddenWord: String
+    var wrongMovesRemainder: Int
+    
+    private var guessedLetters = [Character]()
+    
+    var guessedWord: String {
+        var wordToShow = String()
+        
+        for letter in hiddenWord {
+            if guessedLetters.contains(Character(letter.lowercased())) {
+                wordToShow += String(letter)
+            } else {
+                wordToShow += "_"
+            }
+        }
+        
+        return wordToShow
+    }
+    
+    // MARK: - Interface
+    mutating func playerGuessed(letter: Character) {
+        let lowercasedLetter = Character(letter.lowercased())
+        guessedLetters.append(lowercasedLetter)
+        
+        if !hiddenWord.lowercased().contains(lowercasedLetter) {
+            wrongMovesRemainder -= 1
+        }
+    }
+}
